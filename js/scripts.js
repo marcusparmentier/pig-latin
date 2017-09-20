05f38c09bd4908a651eaa335be64494946d04635
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-09-20 16:37:03 -0700

    Add the second spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 // ****************Business Logic***********************
+var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
+var output = [];
 
 var pigLatin = function(phrase) {
-    return phrase;
+  for (var i = 0; i < vowels.length; i += 1) {
+    if (phrase === vowels[i]) {
+      output = phrase.concat("way");
+      return output;
+    }
+  };
+      output = phrase;
+      return output;
 };
+
 
 
 
@@ -11,8 +21,9 @@ var pigLatin = function(phrase) {
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var phrase = ($("input#phrase").val());
-    var result = pigLatin(phrase);
+    var input = ($("input#phrase").val());
+    var result = pigLatin(input);
+    // console.log(input);
     $("#result").text(result);
   });
 });
